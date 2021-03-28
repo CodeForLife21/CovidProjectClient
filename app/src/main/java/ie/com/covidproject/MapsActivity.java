a8c7c9f00a39509a3b15a019c43b8718cd8028c0
CodeForLife21 <pjbanagher@hotmail.com>
2021-03-28 18:03:19 +0100

    version 2

--- app/src/main/java/ie/com/covidproject/MapsActivity.java
+++ app/src/main/java/ie/com/covidproject/MapsActivity.java
@@ -75,6 +75,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Integer availability, geofenceRadiusSize;
     private String businessTAG, userInput;
     private EditText userInputForGeofenceSize;
+    private String hello;
+
 
 
 
